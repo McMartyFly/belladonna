fd4dd50ce342b601eb8b7a153010bc8e35294d3e
Salar Antinori <>
2018-02-21 10:52:40 -0500

    further css and js bug fixes

--- js/main.js
+++ js/main.js
@@ -2,16 +2,16 @@
 
 	$(document).ready(function () {
 		$(document).on("scroll", onScroll);
- 
+
 		$('a[href^="#"]').on('click', function (e) {
 			e.preventDefault();
 			$(document).off("scroll");
- 
+
 			$('a').each(function () {
 				$(this).removeClass('navactive');
 			})
 			$(this).addClass('navactive');
- 
+
 			var target = this.hash;
 			$target = $(target);
 			$('html, body').stop().animate({
@@ -21,8 +21,32 @@
 				$(document).on("scroll", onScroll);
 			});
 		});
+
+		$('.atg-button').on('click', function(){
+			console.log('nerpl');
+		});
+
+		$(function() {
+			//----- OPEN
+			$('[data-popup-open]').on('click', function(e)  {
+			var targeted_popup_class = jQuery(this).attr('data-popup-open');
+			$('[data-popup="' + targeted_popup_class + '"]').fadeIn(350);
+			e.preventDefault();
+			$('body').addClass('noscroll');
+			});
+			//----- CLOSE
+			$('[data-popup-close]').on('click', function(e)  {
+			var targeted_popup_class = jQuery(this).attr('data-popup-close');
+			$('[data-popup="' + targeted_popup_class + '"]').fadeOut(350);
+			e.preventDefault();
+			$('body').removeClass('noscroll');
+			});
+		});
+
+
+
 	});
- 
+
 	function onScroll(event){
 		var scrollPosition = $(document).scrollTop();
 		$('.nav li a').each(function () {
@@ -36,17 +60,17 @@
 				currentLink.removeClass("navactive");
 			}
 		});
-	
-       
-        $(function(){
-            $('#portfolio').mixitup({
-                targetSelector: '.item',
-                transitionSpeed: 350
-            });
-        });
 
-          $(function() {
-            $( "#datepicker" ).datepicker();
-        });
-    
+
+        // $(function(){
+        //     $('#portfolio').mixitup({
+        //         targetSelector: '.item',
+        //         transitionSpeed: 350
+        //     });
+        // });
+        //
+        //   $(function() {
+        //     $( "#datepicker" ).datepicker();
+        // });
+
     };
